081f0e0e41a4497debb0808a541781b1fda7ff4c
Majd <machlouchmajd1@gmail.com>
2021-11-22 13:56:44 +0100

    values_at method

---
+++ values_at_method.rb
@@ -0,0 +1,9 @@
+# Return an array containing the values associated with the given keys.
+arr = [1,2,3,4,5,6,7,8,9]
+n1, n2, n3 = arr.values_at(0, 1, 2)
+print n1
+# Output: 1
+print n2
+# Output: 2
+print n3
+# Output: 3
